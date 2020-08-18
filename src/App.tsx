b26cdd1fb82ea1b945ce47809e0e12bde83c6583
Rotimi Babalola <tolurotimi@hotmail.com>
2020-08-18 13:16:30 +0200

    Add Dummy dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+// import io from 'socket.io-client';
+
+// const socket = io(':5000');
 
 const App: React.FC = () => (
   <Button color="primary" variant="contained">
---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,33 @@
+// import React from 'react';
+// import { withStyles } from '@material-ui/core/styles';
+// import Paper from '@material-ui/core/Paper';
+// import Typography from '@material-ui/core/Typography';
+
+// const styles = theme => ({
+//   root: {
+//     ...theme.mixins.gutters(),
+//     margin: '50px',
+//     paddingTop: theme.spacing(2),
+//     paddingBottom: theme.spacing(2),
+//   },
+//   flex: {
+//     display: 'flex',
+//     alignItems: 'center',
+//   },
+//   topicsWindow: {
+//     width: '30%',
+//     height: '300px',
+//     borderRight: '1px solid grey',
+//   },
+//   chatWindow: {
+//     width: '70%',
+//     height: '300px',
+//     padding: '20px',
+//   },
+//   chatBox: {
+//     width: '85%',
+//   },
+//   button: {
+//     width: '15%',
+//   },
+// });
